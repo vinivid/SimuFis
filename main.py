b9ecdf17333dfbdb30b994369ec9ca32c882db8c
vini <frato.vini@gmail.com>
2024-11-03 17:51:13 -0300

    comentarios

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from engine.game_engine import Planet
 pygame.init()
 
 engine = GameEngine(1280, 720)
-planet1 = Planet(800000, [400, 500], [10000, 0], [0, 0], 500.0, 5.0, [255, 0, 0, 255])
+#O primeiro planeta vai ser tratado como principal
+planet1 = Planet(80, [400, 500], [10000, 0], [0, 0], 500.0, 5.0, [255, 0, 0, 255])
 planet2 = Planet(800000000000000000000, [600, 400], [0, 0], [0, 0], 500.0, 5.0, [0, 0, 255, 255])
 engine.add_planet(planet1)
 engine.add_planet(planet2)
@@ -14,8 +15,6 @@ engine.add_planet(planet2)
 clock = pygame.time.Clock()
 
 game_loop = True
-
-#o lop conta o prgresso no tempo
 
 while game_loop:
 
@@ -26,6 +25,7 @@ while game_loop:
     engine.update_physics()
     engine.render()
 
+    #TODO: Fazer o calculo do velocity verlet ser independente do tempo
     clock.tick(120)
 
 pygame.quit()
